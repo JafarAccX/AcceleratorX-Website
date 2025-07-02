2062c959f399127aeca3afc8ae395d6457ea648d
satyendra-acc <satyendra@acceleratorx.co>
2025-07-02 17:13:44 +0530

    build

--- dist/assets/browser-DDASWwB4.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:dist/assets/browser-DDASWwB4.js
-import{b8 as e}from"./index-cbYIt0Su.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
-========
-import{b8 as e}from"./index-CQe49drl.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
->>>>>>>> 2916b4eeb167ba447be9bfe3ec537ae87e3edf94:dist/assets/browser-BSqKOZhY.js
