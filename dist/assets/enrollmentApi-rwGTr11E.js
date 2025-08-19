19d0104a98fefb58f14b00284fc0f1b75cf6e2ad
satyendra-acc <satyendra@acceleratorx.co>
2025-08-19 22:12:16 +0530

    env

---
+++ dist/assets/enrollmentApi-rwGTr11E.js
@@ -0,0 +1,1 @@
+import{d as o}from"./index-C0tIxWlc.js";const n=async e=>{try{return(await o.post("/enrollments-new",e)).data}catch(r){throw console.error("Error creating enrollment:",r),r}};export{n as c};
