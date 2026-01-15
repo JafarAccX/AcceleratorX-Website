6d00ba9f4b687ea7d25779b00bd89b8ea189b232
satyendra-acc <satyendra@acceleratorx.co>
2026-01-15 10:41:34 +0530

    allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { visualizer } from 'rollup-plugin-visualizer';
+import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer({ filename: 'dist/bundle-analysis.html', open: true }),],
+  plugins: [
+    react(),
+    visualizer({ filename: "dist/bundle-analysis.html", open: false }),
+  ],
+
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
+
   server: {
-    host: "0.0.0.0",
-    port: 5173,
+    host: true,              // accept external hosts
+    port: 7001,              // MUST match PM2 + Nginx
+    allowedHosts: true       // allow all hosts
   },
+
   build: {
     minify: false,
     rollupOptions: {
       input: {
-        main: './index.html'
-      }
-    }
+        main: "./index.html",
+      },
+    },
   },
+
   ssr: {
-    noExternal: ['react-router-dom', 'react-helmet-async']
-  }
+    noExternal: ["react-router-dom", "react-helmet-async"],
+  },
 });
